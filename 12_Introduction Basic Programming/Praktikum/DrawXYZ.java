cdffeeb156138983472580be5341f3d0e4db9159
melmeeliaa <77379390+melmeeliaa@users.noreply.github.com>
2023-03-10 22:58:15 +0700

    Add files via upload

---
+++ 12_Introduction Basic Programming/Praktikum/DrawXYZ.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DrawXYZ {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input angka   : ");
+        int inputAngka = input.nextInt();
+        int i,j,k = 0;
+        int a = inputAngka;
+        for(i=1; i<=a; i++) {
+            for(j=1; j<=a; j++) {
+                k++;
+                if (k % 3 == 0) {
+                    System.out.print("X");
+                }
+                else if (k % 2 == 0) {
+                    System.out.print("Z");
+                }
+                else {
+                    System.out.print("Y");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
