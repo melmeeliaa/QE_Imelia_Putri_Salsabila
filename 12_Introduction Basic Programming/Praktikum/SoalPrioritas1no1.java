853db02932a9ef00971af0f65de1df0efd508ef8
melmeeliaa <77379390+melmeeliaa@users.noreply.github.com>
2023-03-10 19:32:21 +0700

    Add files via upload

---
+++ 12_Introduction Basic Programming/Praktikum/SoalPrioritas1no1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        //Luas Segitiga
+        System.out.print("Masukkan nilai alas segitiga: ");
+        double alas = input.nextDouble();
+        System.out.print("Masukkan nilai tinggi segitiga: ");
+        double tinggi = input.nextDouble();
+        double luas = 0.5*alas*tinggi;
+        System.out.print("luas segitiga adalah : " + luas);
+
+        //Luas Persegi Panjang
+        System.out.print("Masukkan nilai panjang persegi panjang: ");
+        double panjang = input.nextDouble();
+        System.out.print("Masukkan nilai lebar persegi panjang: ");
+        double lebar = input.nextDouble();
+        double luas2 = panjang*lebar;
+        System.out.print("luas persegi panjang adalah : " + luas2);
+
+        //Luas Lingkaran
+        float phi = 3.14F;
+        System.out.print("Masukkan nilai jari-jari: ");
+        float jari2 = (float) input.nextFloat();
+        double luas3 = phi * jari2 * jari2 ;
+        System.out.println("Luas lingkaran tersebut adalah: " + luas3);
+    }
+}
